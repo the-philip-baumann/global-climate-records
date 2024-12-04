8ea3d5e0701167dd73dcd85d343c75e376327f70
Philip Baumann <philip.baumann@hispeed.ch>
2024-12-04 20:43:29 +0100

    Fix issues

--- main.py
+++ main.py
@@ -8,9 +8,15 @@ from climate_averages import aggregate_average_temperature, \
     aggregate_average_precipitation, aggregate_average_temperature_on_continent_scale, \
     aggregate_average_precipitation_on_continent_scale
 from climate_extremes import calculate_warmest_countries, calculate_coldest_countries
+from climate_detail import calculate_precipitation_mm_for_country, calculate_temperature_for_country
 
 # Streamlint Config
 st.set_page_config(layout="wide")
+if 'selection_box_continent' not in st.session_state:
+    st.session_state.selection_box_continent = 'All'
+
+if 'selection_box_country' not in st.session_state:
+    st.session_state.selection_box_country = 'All'
 
 df_weather = pd.read_parquet('./data/daily_weather.parquet')
 df_weather = df_weather[
@@ -34,36 +40,34 @@ countries = np.insert(countries, 0, 'All')
 df_continents_selections = df_continents.copy()
 continents = np.insert(df_continents_selections, 0, 'All')
 
-st.write('Global Climat Records')
+st.write(f'Global Climat Records - ({st.session_state["selection_box_country"]})')
 
 container = st.container()
 
-col_left, col_right = st.columns(2)
 
 
 def on_change_continent() -> None:
-    selected = st.session_state['selection_box_continent']
-    st.write(f'This is a test: {selected}')
-    # print(selected)
-
+    global countries
+    current_value = st.session_state['selection_box_continent']
+    if not current_value == 'All':
+        countries = df_countries_selections[df_countries_selections['continent'] == current_value][
+            'country'].dropna().unique()
 
 # Filter Form
-with container.form(key='filter_form'):
+continent, country = container.columns(2)
+continent.selectbox(
+    'Continent:',
+    continents,
+    key='selection_box_continent',
+    on_change=on_change_continent()
+)
+country.selectbox(
+    'Country:',
+    countries,
+    key='selection_box_country',
+)
 
-    st.write("Country Filter")
-    continent, country = st.columns(2)
-    continent.selectbox(
-        'Continent:',
-        continents,
-        key='selection_box_continent',
-        on_change=on_change_continent()
-    )
-    country.selectbox(
-        'Country:',
-        countries
-    )
-
-    st.form_submit_button("Search")
+col_left, col_right = st.columns(2)
 
 # Average Temperature
 df_aggregated_average_temp = aggregate_average_temperature(df_countries_cities_weather, dates)
@@ -107,8 +111,29 @@ top_ten_coldest_countries_line_chart = px.line(
     title='Top 10 Coldest Countries',
 )
 
-col_left.plotly_chart(average_precipitation_line_chart)
-col_left.plotly_chart(top_ten_warmest_countries_line_chart)
+df_detail_temperatures = calculate_temperature_for_country(df_countries_cities_weather, st.session_state['selection_box_country'])
+detail_temperatures_line_chart = px.line(
+    df_detail_temperatures,
+    x='date',
+    y='Temperatures',
+    color='Category',
+    title='Temperatures',
+)
 
-col_right.plotly_chart(average_temp_line_chart)
-col_right.plotly_chart(top_ten_coldest_countries_line_chart)
+df_detail_precipitation = calculate_precipitation_mm_for_country(df_countries_cities_weather, st.session_state['selection_box_country'])
+detail_precipitation_line_chart = px.line(
+    df_detail_precipitation,
+    x='date',
+    y='precipitation_mm',
+    title='Precipitation in mm',
+)
+
+if st.session_state.selection_box_country == 'All':
+    col_left.plotly_chart(average_precipitation_line_chart)
+    col_left.plotly_chart(top_ten_warmest_countries_line_chart)
+
+    col_right.plotly_chart(average_temp_line_chart)
+    col_right.plotly_chart(top_ten_coldest_countries_line_chart)
+else:
+    col_left.plotly_chart(detail_temperatures_line_chart)
+    col_right.plotly_chart(detail_precipitation_line_chart)
